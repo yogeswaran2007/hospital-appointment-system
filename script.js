4b408a1ae7de11120938dac3f7b1d2289714f6ef
Yogeswaran <yogesvetri2007@gmail.com>
2026-06-02 11:59:35 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,33 +1,26 @@
-const form =
-document.getElementById("appointmentForm");
+const form = document.getElementById("appointmentForm");
+const list = document.getElementById("appointmentList");
 
-const list =
-document.getElementById("appointmentList");
+form.addEventListener("submit", function(event){
 
-form.addEventListener("submit", function(e){
+    event.preventDefault();
 
-e.preventDefault();
+    let name = document.getElementById("name").value;
+    let age = document.getElementById("age").value;
+    let doctor = document.getElementById("doctor").value;
+    let date = document.getElementById("date").value;
 
-let name =
-document.getElementById("name").value;
+    let li = document.createElement("li");
 
-let age =
-document.getElementById("age").value;
+    li.textContent =
+    "Name: " + name +
+    " | Age: " + age +
+    " | Doctor: " + doctor +
+    " | Date: " + date;
 
-let doctor =
-document.getElementById("doctor").value;
+    list.appendChild(li);
 
-let date =
-document.getElementById("date").value;
+    alert("Appointment Booked Successfully! SMS and Email Notification Sent.");
 
-let li =
-document.createElement("li");
-
-li.textContent =
-`${name} | Age:${age} | ${doctor} | ${date}`;
-
-list.appendChild(li);
-
-form.reset();
-
+    form.reset();
 });
