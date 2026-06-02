e2d35be6214e80ddf605c077c8f9e807dbc43e8a
Yogeswaran <yogesvetri2007@gmail.com>
2026-06-02 12:59:00 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+let token = 1001;
+
 const form = document.getElementById("appointmentForm");
 const list = document.getElementById("appointmentList");
 
@@ -5,32 +7,37 @@ form.addEventListener("submit", function(event){
 
     event.preventDefault();
 
-    let name =
-    document.getElementById("name").value;
+    let name = document.getElementById("name").value;
+    let mobile = document.getElementById("mobile").value;
+    let email = document.getElementById("email").value;
+    let age = document.getElementById("age").value;
+    let doctor = document.getElementById("doctor").value;
+    let date = document.getElementById("date").value;
 
-    let age =
-    document.getElementById("age").value;
+    let currentToken = token;
 
-    let doctor =
-    document.getElementById("doctor").value;
+    let li = document.createElement("li");
 
-    let date =
-    document.getElementById("date").value;
-
-    let li =
-    document.createElement("li");
-
-    li.textContent =
-    "Patient: " + name +
-    " | Age: " + age +
-    " | Doctor: " + doctor +
-    " | Date: " + date;
+    li.innerHTML =
+    "<b>Token No:</b> " + currentToken +
+    "<br><b>Name:</b> " + name +
+    "<br><b>Mobile:</b> " + mobile +
+    "<br><b>Email:</b> " + email +
+    "<br><b>Age:</b> " + age +
+    "<br><b>Doctor:</b> " + doctor +
+    "<br><b>Date:</b> " + date;
 
     list.appendChild(li);
 
     alert(
-    "Appointment Booked Successfully!\nSMS and Email Notification Sent."
+        "Appointment Confirmed!\n\n" +
+        "Hospital: Vetri Vel Multi Speciality Hospital\n" +
+        "Token Number: " + currentToken +
+        "\nSMS Notification Sent" +
+        "\nEmail Notification Sent"
     );
+
+    token++;
 
     form.reset();
 });
